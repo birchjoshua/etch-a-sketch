1694322b2e4b28164974943d88746f2088835f13
Joshua Birch <snowpigdog@gmail.com>
2017-06-26 20:08:17 -0600

    On mouseenter works

--- etch.js
+++ etch.js
@@ -1,5 +1,8 @@
 $(document).ready(function(){
 	for(i=0; i<=256; i++){
 		$('.container').append('<div class="pixels"></div>')};
+	$('.pixels').on('mouseenter', function() {
+		$(this).addClass('hovered')
+	})
   
 })
