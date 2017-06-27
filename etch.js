c8b9c71a69d741229dff2ee52643b36c695fbbdb
Joshua Birch <snowpigdog@gmail.com>
2017-06-27 08:32:03 -0600

    Finished for now, optional tasks remain.

--- etch.js
+++ etch.js
@@ -2,12 +2,19 @@ $(document).ready(function(){
 	for(i=0; i<=256; i++){
 		$('.container').append('<div class="pixels"></div>')};
 	$('.pixels').on('mouseenter', function() {
-		$(this).addClass('hovered')
+		$(this).addClass('hovered');
 	})
   	$('.refresh').on('click', function() {
-  		$('.pixels').remove()
-  		var square = prompt("Enter the number of pixels to square:")
-  		var squareInt = Number(square)
-  		
-  	})
+  		$('.pixels').remove();
+  		var square = prompt("Enter the number of pixels to square:");
+  		var squareInt = Number(square);
+  		for(i=0; i<=squareInt*squareInt; i++){
+			$('.container').append('<div class="pixels"></div>')};
+		$('.pixels').css('height', 800/squareInt);
+		$('.pixels').css('width', 800/squareInt);
+		$('.pixels').on('mouseenter', function() {
+			$(this).addClass('hovered');
+		})
+	})
+	
 })
